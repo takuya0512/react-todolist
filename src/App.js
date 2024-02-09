a282688aac0e964225e48c4882492e7664a87f0e
takuya0512 <ta98euleka@gmail.com>
2024-02-09 16:38:29 +0900

    機能実装

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import './App.css';
+import { useState, useRef } from "react";
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState([]);
+
+  const todoNameRef = useRef();
+
+  const handleaddTodo = () => {
+    // タスクを追加
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+  
+  return (
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleaddTodo}>タスクを追加</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
+    </>
+  );
 }
 
 export default App;
